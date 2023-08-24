9a5eb94cf3bc3f2698cede98ee0bd6bae7d480cf
Ferdinand Mom <ferdinand.mom@epita.fr>
2023-08-24 09:46:25 +0000

    update

--- first.sh
+++ first.sh
@@ -11,14 +11,13 @@ sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/ins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-
 # Link zsh
 rm $HOME/.zshrc
 ln -s $HOME/dotfiles-vastai/config/zshrc $HOME/.zshrc
 
 rm -rf $HOME/.zsh
 mkdir -p $HOME/.zsh
-ln -s $HOME/dotfiles/config/aliases.zsh $HOME/.zsh/aliases.zsh
+ln -s $HOME/dotfiles-vastai/config/aliases.zsh $HOME/.zsh/aliases.zsh
 
 # Change shell to zsh
 chsh -s $(which zsh)
