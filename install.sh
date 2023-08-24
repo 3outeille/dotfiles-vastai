1b8e1bf8c2020e0c21860f53513a5b428b9c70e9
Ferdinand MOM <ferdinand.mom@epita.fr>
2023-08-24 11:34:25 +0200

    init

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
+# ========== ssh ==========
+
+ssh-keygen -t ed25519 -C "ferdinand.mom@epita.fr"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+
 # ========== ZSH ==========
 sudo apt install zsh
 # install oh-my-zsh
