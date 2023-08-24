bd25a1c5295e953172a21399bcd78bd348cbb545
root <ferdinand.mom@epita.fr>
2023-08-24 09:39:27 +0000

    update

--- first.sh
+++ first.sh
@@ -8,6 +8,9 @@ ssh-add ~/.ssh/id_ed25519
 sudo apt install zsh
 # install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 
 # Link zsh
 rm $HOME/.zshrc
@@ -19,19 +22,3 @@ ln -s $HOME/dotfiles/config/aliases.zsh $HOME/.zsh/aliases.zsh
 
 # Change shell to zsh
 chsh -s $(which zsh)
-
-# ======= Pdbpp =========
-rm $HOME/.pdbrc.py
-rm $HOME/.pdbrc
-
-ln -s $HOME/dotfiles/config/pdbrc.py $HOME/.pdbrc.py
-ln -s $HOME/dotfiles/config/pdbrc $HOME/.pdbrc
-
-
-# ====== Fuzzer Finder =====
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-
-# ======= Pyenv =======
-curl https://pyenv.run | bash
---
+++ second.sh
@@ -0,0 +1,13 @@
+# ====== Fuzzer Finder =====
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# ======= Pdbpp =========
+rm $HOME/.pdbrc.py
+rm $HOME/.pdbrc
+
+ln -s $HOME/dotfiles/config/pdbrc.py $HOME/.pdbrc.py
+ln -s $HOME/dotfiles/config/pdbrc $HOME/.pdbrc
+
+# ======= Pyenv =======
+curl https://pyenv.run | bash
