2083b51595fb7e04a431ea774e686abd5e3cd26e
Ferdinand Mom <47445085+3outeille@users.noreply.github.com>
2023-08-24 12:06:58 +0200

    Update second.sh

--- second.sh
+++ second.sh
@@ -1,5 +1,17 @@
 sudo apt install vim
 
+# ======= ZSH =========
+# Link zsh
+rm $HOME/.zshrc
+ln -s $HOME/dotfiles-vastai/config/zshrc $HOME/.zshrc
+
+rm -rf $HOME/.zsh
+mkdir -p $HOME/.zsh
+ln -s $HOME/dotfiles-vastai/config/aliases.zsh $HOME/.zsh/aliases.zsh
+
+# Change shell to zsh
+chsh -s $(which zsh)
+
 # ====== Fuzzer Finder =====
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
