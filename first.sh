293581db6902049667cb80390b298ae52a05dd2e
Ferdinand Mom <47445085+3outeille@users.noreply.github.com>
2023-08-24 12:06:39 +0200

    Update first.sh

--- first.sh
+++ first.sh
@@ -8,16 +8,8 @@ ssh-add ~/.ssh/id_ed25519
 sudo apt install zsh
 # install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# Link zsh
-rm $HOME/.zshrc
-ln -s $HOME/dotfiles-vastai/config/zshrc $HOME/.zshrc
-
-rm -rf $HOME/.zsh
-mkdir -p $HOME/.zsh
-ln -s $HOME/dotfiles-vastai/config/aliases.zsh $HOME/.zsh/aliases.zsh
-
-# Change shell to zsh
-chsh -s $(which zsh)
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git 
+git clone https://github.com/zsh-users/zsh-autosuggestions 
+mv zsh-syntax-highlighting ${HOME}/.oh-my-zsh/plugins
+mv zsh-autosuggestions ${HOME}/.oh-my-zsh/plugins
