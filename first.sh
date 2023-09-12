6d5749aa32670ba7e73d52858114aa5a7ed47118
Ferdinand Mom <ferdinand.mom@epita.fr>
2023-09-12 16:44:45 +0000

    git global

--- first.sh
+++ first.sh
@@ -4,6 +4,9 @@ ssh-keygen -t ed25519 -C "ferdinand.mom@epita.fr"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
 
+git config --global user.email "ferdinand.mom@epita.fr"
+git config --global user.name "Ferdinand Mom"
+
 # ========== ZSH ==========
 sudo apt install zsh
 # install oh-my-zsh
