6295a9806054bb104b930d29c66046dbac385e9b
Ferdinand Mom <ferdinand.mom@epita.fr>
2023-09-12 17:04:51 +0000

    add apt install for tiktoken

--- second.sh
+++ second.sh
@@ -29,6 +29,7 @@ ln -s $HOME/dotfiles/config/pdbrc $HOME/.pdbrc
 # ======= Pyenv =======
 curl https://pyenv.run | bash
 sudo apt-get install build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev
+sudo apt-get install python3-zipp # for tiktoken
 
 # ======== Nvtop ======
 sudo apt install nvtop
