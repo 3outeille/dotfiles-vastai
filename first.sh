f67669f60de862fdc2d6a3a46603b54a396b5a8c
Ferdinand Mom <ferdinand.mom@epita.fr>
2023-11-11 14:36:30 +0000

    update vscode

--- first.sh
+++ first.sh
@@ -1,11 +1,11 @@
 # ========== ssh ==========
 
-ssh-keygen -t ed25519 -C "ferdinand.mom@epita.fr"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
+# ssh-keygen -t ed25519 -C "ferdinand.mom@epita.fr"
+# eval "$(ssh-agent -s)"
+# ssh-add ~/.ssh/id_ed25519
 
-git config --global user.email "ferdinand.mom@epita.fr"
-git config --global user.name "Ferdinand Mom"
+# git config --global user.email "ferdinand.mom@epita.fr"
+# git config --global user.name "Ferdinand Mom"
 
 # ========== ZSH ==========
 yes | sudo apt install zsh
