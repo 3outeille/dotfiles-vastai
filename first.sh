cb881af9dd4ae54e23645ceae88c325dd6497756
Ferdinand MOM <ferdinand.mom@epita.fr>
2023-11-11 15:02:11 +0100

    add yes

--- first.sh
+++ first.sh
@@ -8,6 +8,6 @@ git config --global user.email "ferdinand.mom@epita.fr"
 git config --global user.name "Ferdinand Mom"
 
 # ========== ZSH ==========
-sudo apt install zsh
+yes | sudo apt install zsh
 # install oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
