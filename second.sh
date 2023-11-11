d29196dfdfda7e1a9519cfbdf54e3d2a083eedea
Ferdinand Mom <47445085+3outeille@users.noreply.github.com>
2023-11-11 14:49:40 +0100

    Update second.sh

--- second.sh
+++ second.sh
@@ -10,14 +10,14 @@ mkdir -p $HOME/.zsh
 ln -s $HOME/dotfiles-vastai/config/aliases.zsh $HOME/.zsh/aliases.zsh
 
 # Change shell to zsh
-chsh -s $(which zsh)
+yes | chsh -s $(which zsh)
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 mv zsh-syntax-highlighting ${HOME}/.oh-my-zsh/plugins
 
 # ====== Fuzzer Finder =====
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+yes | ~/.fzf/install
 
 # ======= Pdbpp =========
 rm $HOME/.pdbrc.py
@@ -28,10 +28,16 @@ ln -s $HOME/dotfiles-vastai/config/pdbrc $HOME/.pdbrc
 
 # ======= Pyenv =======
 curl https://pyenv.run | bash
-sudo apt-get install build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev
-sudo apt-get install python3-zipp # for tiktoken
-sudo apt-get install python3-dev  # for torch-compile
+yes | sudo apt-get install build-essential zlib1g-dev libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev
+yes | sudo apt-get install python3-zipp # for tiktoken
+yes | sudo apt-get install python3-dev  # for torch-compile
 
 # ======== Nvtop ======
-sudo apt install nvtop
+yes | sudo apt install nvtop
 
+# ====== Expman =========
+git clone https://github.com/megvii-research/expman $HOME/.expman
+echo '[[ -o interactive ]] && source $HOME/.expman/expman.zsh' >> ~/.zshrc
+
+# ======= vscode extension ==========
+ln -s $HOME/dotfiles-vastai/config/vscode-server/extensions $HOME/.vscode-server/extensions
